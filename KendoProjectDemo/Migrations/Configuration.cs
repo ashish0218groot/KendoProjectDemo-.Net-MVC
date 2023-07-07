19e92a51aa30b05f373d90a4f3a5828b396e9ad9
Harsh Kaushal <harshkaushal@gmail.com>
2023-07-07 15:51:02 +0530

    Add project files.

---
+++ KendoProjectDemo/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace KendoProjectDemo.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<KendoProjectDemo.Models.EmployeeDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(KendoProjectDemo.Models.EmployeeDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ KendoProjectDemo/Models/DataModel/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KendoProjectDemo.Models.DataModel
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public string OrderNumber { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        // Foreign key to the associated employee
+        public Guid EmployeeId { get; set; }
+        public Employees Employee { get; set; }
+    }
+}
