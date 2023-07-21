d6811268cb2cbb587fe948acb0dcb94c779774a6
Harsh Kaushal <harshkaushal@gmail.com>
2023-07-21 17:03:10 +0530

    Using various ways to transfer data from view to controller, using Parameter,Request, Formdata, ModelBinding

---
+++ KendoProjectDemo/Controllers/TestController.cs
@@ -0,0 +1,45 @@
+﻿using KendoProjectDemo.Models.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoProjectDemo.Controllers
+{
+    public class TestController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string PostUsingParameters(string FirstName, string LastName)
+        {
+            return "Post Using Parameters   -" + FirstName + "," + LastName;
+        }
+
+        [HttpPost]
+        public string PostUsingRequest()
+        {
+            string FirstName = Request["FirstName"];
+            string LastName = Request["LastName"];
+            return "Post Using Request -" + FirstName + "," + LastName;
+        }
+
+        [HttpPost]
+        public string PostUsingFormCollection(FormCollection form)
+        {
+            string FirstName = form["FirstName"];
+            string LastName = form["LastName"];
+            return "Post Using Request -" + FirstName + "," + LastName;
+        }
+
+        [HttpPost]
+        public string PostUsingModelBinding(Employees employees)
+        {  
+            return "Post Using ModelBinding -" + employees.FirstName + "," + employees.LastName;
+        }
+    }
+}
